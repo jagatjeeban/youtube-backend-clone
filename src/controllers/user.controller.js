3556ed724eb74b4232f19181bfccdc9a5e4fa9a2
Jagat Jeeban Maharana <jagatjeebanmaharana@gmail.com>
2025-09-06 22:31:13 +0530

    refactor: update logOutUser to unset refreshToken instead of setting it to null

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -146,8 +146,8 @@ const logOutUser = asyncHandler(async (req, res) => {
     await User.findByIdAndUpdate(
         req.user._id,
         {
-            $set: {
-                refreshToken: null
+            $unset: {
+                refreshToken: 1 //this removes the field from the document
             }
         },
         { new: true }
