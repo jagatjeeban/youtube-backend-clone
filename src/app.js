c85a4907db503e429902dd539a1c06fec3a96c0c
Jagat Jeeban Maharana <jagatjeebanmaharana@gmail.com>
2025-09-06 23:23:04 +0530

    feat: add routes and empty controllers for comments, likes, playlists, subscriptions, tweets, and videos

--- src/app.js
+++ src/app.js
@@ -5,6 +5,12 @@ import { LIMIT } from './constants.js';
 
 //import routes
 import userRoutes from './routes/user.route.js';
+import tweetRouter from "./routes/tweet.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
 
 const app = express();
 
@@ -25,5 +31,11 @@ app.use(cookieParser());
 
 //routes declaration
 app.use('/api/v1/users', userRoutes);
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
 
 export { app }
