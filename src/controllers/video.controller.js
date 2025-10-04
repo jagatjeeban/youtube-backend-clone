c0c1880b5d4df1d405143c21bd911fa2a27fb789
Jagat Jeeban Maharana <jagatjeebanmaharana@gmail.com>
2025-10-05 00:50:54 +0530

    feat: implement validation and response handling in getVideoById function

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -18,8 +18,23 @@ const publishAVideo = asyncHandler(async (req, res) => {
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: get video by id
+    const { videoId } = req.params;
+
+    if (!videoId) {
+        throw new ApiError(400, 'Video Id is missing!')
+    }
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, 'Video Id is invalid!')
+    }
+
+    const video = await Video.findById(videoId);
+    if (!video) {
+        throw new ApiError(404, 'Video does not exist!')
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, video, 'Video fetched successfully!')
+    )
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
